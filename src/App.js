b8f6228c66bb8cd3ce37ae034f339b171e895d0e
Kelin Wu <kelin.wu@sas.com>
2020-10-19 16:12:14 -0400

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { StyledModal } from "./reuseableModal";
 
 function App() {
+  const [isPopOpen, setIsPosOpen] = React.useState(false)
+  const [isPopOpen2, setIsPosOpen2] = React.useState(false)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <span
+          style={{
+            display: 'flex',
+            margin: '5rem',
+            cursor: 'pointer'
+          }}
+          onClick={() => setIsPosOpen(!isPopOpen)}
         >
-          Learn React
-        </a>
+          Click to pop modal window #1
+      </span>
+        <StyledModal
+          show={isPopOpen}
+          handleClose={() => setIsPosOpen(false)}
+        >
+          <div style={{ color: "black" }}>
+
+            <h1>Pop Window #1 </h1>
+              1) reuseable styled component
+              2) easy implement
+              </div>
+
+        </StyledModal>
+
+        <span
+          style={{
+            display: 'flex',
+            margin: '5rem',
+            cursor: 'pointer'
+          }}
+          onClick={() => setIsPosOpen2(!isPopOpen2)}
+        >
+          Click to pop modal window #2
+      </span>
+        <StyledModal
+          show={isPopOpen2}
+          handleClose={() => setIsPosOpen2(false)}
+        >
+          <div style={{ color: "black" }}>
+
+            <h1>Pop Window #1 </h1>
+              1) could be very stylish
+              2) can use throughout of entire application
+              </div>
+
+        </StyledModal>
+
+
       </header>
     </div>
   );
